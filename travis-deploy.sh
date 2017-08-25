752e763cea1135803092ccabab8106a6f209c712
Christopher Jones <tophj@linux.vnet.ibm.com>
2017-08-25 12:53:38 -0500

    uncommenting wininspector
    Signed-off-by: Christopher Jones <tophj@linux.vnet.ibm.com>

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -7,25 +7,25 @@ image="tophj/whoami"
 docker tag whoami "$image:linux-$ARCH-$TRAVIS_TAG"
 docker push "$image:linux-$ARCH-$TRAVIS_TAG"
 
-#if [ "$ARCH" == "amd64" ]; then
-#  set +e
-#  echo "Waiting for other images $image:linux-arm-$TRAVIS_TAG"
-#  until docker run --rm stefanscherer/winspector "$image:linux-arm-$TRAVIS_TAG"
-#  do
-#    sleep 15
-#    echo "Try again"
-#  done
-#  until docker run --rm stefanscherer/winspector "$image:linux-arm64-$TRAVIS_TAG"
-#  do
-#    sleep 15
-#    echo "Try again"
-#  done
-#  until docker run --rm stefanscherer/winspector "$image:windows-amd64-$TRAVIS_TAG"
-#  do
-#    sleep 15
-#    echo "Try again"
-#  done
-#  set -e
+if [ "$ARCH" == "amd64" ]; then
+  set +e
+  echo "Waiting for other images $image:linux-arm-$TRAVIS_TAG"
+  until docker run --rm stefanscherer/winspector "$image:linux-arm-$TRAVIS_TAG"
+  do
+    sleep 15
+    echo "Try again"
+  done
+  until docker run --rm stefanscherer/winspector "$image:linux-arm64-$TRAVIS_TAG"
+  do
+    sleep 15
+    echo "Try again"
+  done
+  until docker run --rm stefanscherer/winspector "$image:windows-amd64-$TRAVIS_TAG"
+  do
+    sleep 15
+    echo "Try again"
+  done
+  set -e
 
   echo "Downloading docker client with manifest command"
   wget https://4242-88013053-gh.circle-artifacts.com/1/work/build/docker-linux-amd64
