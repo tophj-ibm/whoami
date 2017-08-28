f23ada4cf661f288735b1bbf186474c56ef8779f
Christopher Jones <tophj@linux.vnet.ibm.com>
2017-08-28 14:29:50 -0500

    need more sleep
    Signed-off-by: Christopher Jones <tophj@linux.vnet.ibm.com>

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -7,10 +7,10 @@ echo "Installing certs"
 sudo apt-get install -y openssl
 sudo openssl s_client -connect $TARGET_REGISTRY:5000 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM | sudo tee /usr/local/share/ca-certificates/$TARGET_REGISTRY.crt
 sudo update-ca-certificates
-sudo service docker restart
 sudo mkdir -p /etc/docker/certs.d/$TARGET_REGISTRY:5000
 sudo openssl s_client -connect $TARGET_REGISTRY:5000 -showcerts </dev/null 2>/dev/null | sudo openssl x509 -outform PEM | sudo tee /etc/docker/certs.d/$TARGET_REGISTRY:5000/ca.crt
 sudo service docker restart
+sleep 7
 
 echo "Pushing inital images"
 
