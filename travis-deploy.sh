360af7be026abe9e47fb55332163e96c870699e3
Christopher Jones <tophj@linux.vnet.ibm.com>
2017-08-28 13:16:44 -0500

    added cert stuff
    Signed-off-by: Christopher Jones <tophj@linux.vnet.ibm.com>

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-docker images
-
 image="$TARGET_REGISTRY/tophj/whoami"
 docker tag whoami "$image:linux-$ARCH-$TRAVIS_TAG"
 docker push "$image:linux-$ARCH-$TRAVIS_TAG"
@@ -35,6 +33,15 @@ if [ "$ARCH" == "amd64" ]; then
   chmod +x docker
   ./docker version
   
+
+  # installing cert
+  apt-get install -y openssl
+  mkdir -p /etc/docker/certs.d/$TARGET_REGISTRY:5000
+  openssl s_client -connect $TARGET_REGISTRY:5000 -showcerts </dev/null 2>/dev/null | openssl x509 -outform PEM | tee /etc/docker/certs.d/$TARGET_REGISTRY:5000/ca.crt
+  /bin/systemctl restart docker.service
+
+
+
   set -x
   
   echo "Pushing manifest $image:$TRAVIS_TAG"
